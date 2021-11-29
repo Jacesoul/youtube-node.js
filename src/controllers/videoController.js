b553d82b9d1f84938da03a0f37bb755f59975751
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-11-29 12:00:18 +0900

    findByIdAndUpdate / exists 메소드

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -17,6 +17,7 @@ export const home = async (req, res) => {
     return res.render("server-error");
   }
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params; // const id = req.params.id와 같다.
   const video = await Video.findById(id);
@@ -25,6 +26,7 @@ export const watch = async (req, res) => {
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
+
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -33,25 +35,28 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit ${video.title} `, video });
 };
+
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
