bb35dc81d510ca3387220b5b0952a1703ffd7712
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-11-29 10:55:36 +0900

    스키마 옵션 type, required, default, trim, maxlength, minlength

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true }, // { type: String }과 같은 의미이다.
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxlength: 80 },
+  description: { type: String, required: true, trim: true, minlength: 20 },
   createdAt: { type: Date, required: true, default: Date.now }, // Date.now()를 하지 않은 이유는 바로 실행시키고 싶지 않아서이다. mongoose는 새로운 video를 생성했을 때만 실행시킨다.
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
