e829b85ee34ab3becae96e36c7f2d6d1890a7393
Jace <jace@Jaceui-iMac.local>
2021-11-30 22:45:12 +0900

    사용하지 않는 import 정리

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
