697322a93c96dd5c38bf60dfe60e294f7223ab30
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-01 01:53:55 +0900

    유저프로필 수정을 위한 template, router, controller 생성

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
-  edit,
   logout,
   see,
   startGithubLogin,
   finishGithubLogin,
+  getEdit,
+  postEdit,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin); // 해당 URL은 Github 사이트에서 설정가능
 userRouter.get("/:id", see);
