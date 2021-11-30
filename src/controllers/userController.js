fa5821dc1412a9005994d2b580f3b00c6de6a88b
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-11-30 16:22:32 +0900

    Github application register / 깃헙로그인 URL 생성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -55,6 +55,19 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = `https://github.com/login/oauth/authorize`;
+  const config = {
+    client_id: "c1c4d6b99499b9cb67a0",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
 export const logout = (req, res) => res.send("Logout User");
