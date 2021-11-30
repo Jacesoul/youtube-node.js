0321dc967a773f535d6478458b2cbfe541f57576
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-01 03:24:36 +0900

    privateOnlyMiddleware, publicOnlyMiddleware 미들웨어 생성 및 적용

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,18 @@ import {
   getEdit,
   postEdit,
 } from "../controllers/userController";
+import { privateOnlyMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin); // 해당 URL은 Github 사이트에서 설정가능
+userRouter.get("/logout", privateOnlyMiddleware, logout);
+userRouter
+  .route("/edit")
+  .all(privateOnlyMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin); // 해당 URL은 Github 사이트에서 설정가능
 userRouter.get("/:id", see);
 
 export default userRouter;
