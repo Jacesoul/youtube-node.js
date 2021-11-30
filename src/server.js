1d6dd50ec2da180bd7891b6f4b20b09eaf20f6d7
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-11-30 14:58:13 +0900

    세션을 mongoDB에 저장(MongoStore)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -19,6 +20,7 @@ app.use(
     secret: "Hello!",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }), // 이 부분의 코드가 없다면 세션은 서버메모리에 저장이된다.그러면 서버를 재시작 할때마다 메모리가 지워지게된다.
   })
 );
 
