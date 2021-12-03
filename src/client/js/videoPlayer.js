4495684fc77723f3d4af64f2ef23766430ce3b93
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-03 12:35:41 +0900

    Timeline 기능 생성

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,9 +4,12 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
+let timelineValue = 0;
+video.duration = timelineValue;
 
 const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substr(14, 5);
@@ -40,15 +43,25 @@ const handleVolumeChange = (event) => {
   volumeValue = value;
   video.volume = value;
 };
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  timelineValue = value;
+  video.currentTime = value;
+};
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
 };
 const handleTimeUpdate = () => {
   currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
 };
 
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+timeline.addEventListener("input", handleTimelineChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
