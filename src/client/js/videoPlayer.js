05f5ca03b3bfe3000f17287e38f55827e86ea5bc
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-03 10:59:45 +0900

    Play / Pause 기능 생성

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,22 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlay = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePauseClick = (e) => (playBtn.innerText = "Play");
+const handlePlayClick = (e) => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePauseClick);
+video.addEventListener("play", handlePlayClick);
