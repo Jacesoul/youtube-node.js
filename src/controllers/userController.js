b0be139be7018c2ca054191307d869f3dbd4db16
Jace <jace@Jaceui-iMac.local>
2021-12-07 09:34:25 +0900

    [Bugfix] 마이페이지에서 썸네일보이지 않는부분 이미지주소 절대경로로 해결

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -132,8 +132,8 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  req.session.destroy();
   req.flash("info", "bye bye");
+  req.session.destroy();
   return res.redirect("/");
 };
 
