3fd9a5d44272bd8fc07641f77a18673fea2dc39b
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-07 15:14:39 +0900

    Rendering Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,7 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
@@ -9,12 +9,13 @@ const handleSubmit = (event) => {
   if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" }, // express.json()에게 json으로 보내고 있다고 알려줘야 한다.
     body: JSON.stringify({ text }), // JSON을 string으로 변환해서 서버에 전송
   });
   textarea.value = "";
+  window.location.reload(); // 댓글을 달때마다 페이지를 새로고침한다. 그렇게 되면 매 댓글에 DB에서 동영상을 찾게 되어 부하가 걸리는 작업일수 있다.
 };
 if (form) {
   form.addEventListener("submit", handleSubmit);
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,7 +15,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params; // const id = req.params.id와 같다.
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
@@ -146,6 +146,7 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
-
+  video.comments.push(comment._id);
+  await video.save();
   return res.sendStatus(201);
 };
